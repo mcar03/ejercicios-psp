d330a7384f27261f49d20e8fac2bfda676308c97
mcar03 <mcararm1311@g.educaand.es>
2024-10-03 14:13:34 +0200

    Dos partes terminadas

--- Tarea5.java
+++ Tarea5.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Tarea5
@@ -6,8 +9,26 @@ import java.io.IOException;
 public class Tarea5 {
 
     public static void main(String[] args) {
-        try {
+      /*/  try {
             Process p = Runtime.getRuntime().exec("mspaint");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+*/
+           try {
+
+            Process p = Runtime.getRuntime().exec("cmd.exe /c dir C:\\Users\\mañana\\Desktop");
+            InputStream is = p.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String linea; 
+
+            while ((linea = br.readLine())!=null) {
+                System.out.println(linea);
+            }
+            System.out.println("Acabado de leer");
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
